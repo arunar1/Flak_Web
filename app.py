cca371050192579065da8a40e9922d4c3bfb515e
Arun <arunarofficial00@gmail.com>
2024-05-24 14:07:19 +0530

    feat : added templates

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app =  Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "welcome"
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 
 @app.route('/about')
 def about():
-    return 'About Page'
+    return render_template('about.html')
 
 if __name__== '__main__':
     app.run(debug=True)
